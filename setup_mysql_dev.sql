e42b5024ac518f02766eb5b144f3d0d480c0d68a
bonnett93 <bonnettjordan@gmail.com>
2021-08-10 11:35:30 -0500

    Fix line 8, db name

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,7 +5,10 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant all privileges for dev user in dev database
-GRANT ALL PRIVILEGES ON hbnb_dev.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Grant select privileges for dev user in performance_schema database
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Reload privileges
+FLUSH PRIVILEGES;
