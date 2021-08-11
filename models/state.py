778abf82a592e512dceb470fd6c1b9147b180dbc
faykris <faykris28@gmail.com>
2021-08-11 12:41:08 -0500

    update City, State, User, Places, - add fields definitions

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from models import storage_type
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -7,6 +8,10 @@ from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    states = relationship("City", backref='states')
+    if storage_type == 'db':
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        states = relationship("City", backref='state')
+    else:
+        state_id = ""
+        name = ""
