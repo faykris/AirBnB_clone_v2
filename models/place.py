602d375adb2d01161c651b5f48f8ce8f7aa5c2db
bonnett93 <bonnettjordan@gmail.com>
2021-08-12 23:42:43 -0500

    Add utilies task 9

--- models/place.py
+++ models/place.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+from models.review import Review
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
@@ -11,29 +12,29 @@ storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
     if storage_type == 'db':
-        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-        name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=True)
-        number_rooms = Column(Integer, nullable=False, default=0)
-        number_bathrooms = Column(Integer, nullable=False, default=0)
-        max_guest = Column(Integer, nullable=False, default=0)
-        price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float)
-        longitude = Column(Float)
-        # places = relationship("Place", backref='user',
-        #                      cascade="all, delete, delete-orphan")
-        # reviews = relationship("Review", backref='place')
+        reviews = relationship("Review",
+                               backref="places",
+                               cascade="all, delete, delete-orphan")
     else:
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
-        amenity_ids = []
+        @property
+        def reviews(self):
+            """returns the list of Review instances with place_id
+                equals to the current Place.id"""
+            from models import storage
+            tmp_reviews = []
+            all_reviews = storage.all(Review)
+            for review in all_reviews.values():
+                if review.place_id == self.id:
+                    tmp_reviews.append(review)
+            return
--- models/state.py
+++ models/state.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
 from models.city import City
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
@@ -16,17 +15,18 @@ class State(BaseModel, Base):
 
     if os.environ.get('HBNB_TYPE_STORAGE') == "db":
         cities = relationship("City",
-                                backref="states",
-                                cascade="all, delete, delete-orphan")
+                              backref="states",
+                              cascade="all, delete, delete-orphan")
 
     else:
         @property
         def cities(self):
             """ Returns the list of City instances with
             state_id == current State.id """
-            all_cities = models.storage.all(City)
-            state_cities = []
-            for city_ins in all_cities.values():
-                if city_ins.state_id == self.id:
-                    state_cities.append(city_ins)
-            return state_cities
+            from models import storage
+            tmp_cities = []
+            all_cities = storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    tmp_cities.append(city)
+            return tmp_cities
