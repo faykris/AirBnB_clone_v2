5aad243c51f8ac7aed771947a06eb56550a65adf
faykris <faykris28@gmail.com>
2021-08-26 17:35:57 -0500

    add 1-pack_web_static.py - try with Holberton checker

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""1. Compress before sending - module"""
+
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """do_pack - function"""
+
+    try:
+        local("mkdir -p versions")
+        n_time = datetime.now()
+        s_time = str(n_time.year).zfill(4) + str(n_time.month).zfill(2) +\
+            str(n_time.day).zfill(2) + str(n_time.hour).zfill(2) +\
+            str(n_time.minute).zfill(2) + str(n_time.second).zfill(2)
+        tgz_path_file = "versions/web_static_{}.tgz".format(s_time)
+        local("tar -czvf {} web_static".format(tgz_path_file))
+        return tgz_path_file
+    except:
+        return None
