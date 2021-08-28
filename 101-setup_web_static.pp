5c60e46c3ec28a312d3c04d1cf14ca1d90b7edd9
faykris <faykris28@gmail.com>
2021-08-28 18:38:30 -0500

    update 101-setup_web_static.pp - join all in one function

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,35 +1,7 @@
 # 5. Puppet for setup
 # Redo the task #0 but by using Puppet:
 
-exec { 'nginx_install':
-  command  => 'apt-get -y update; apt-get -y install nginx',
-  provider => shell,
-}
-exec { 'make_directories':
-  command  => 'mkdir -p /data/web_static/releases/; mkdir -p /data/web_static/shared/; mkdir -p /data/web_static/releases/test/;',
-  provider => shell,
-}
-exec { 'move_index_page':
-  command  => 'echo "<html><head></head><body><h1>Hello Page!</h1></body></html>" > index.html; mv index.html /data/web_static/releases/test/',
-  provider => shell,
-}
-exec { 'create_symlink':
-  command  => 'ln -sf /data/web_static/releases/test /data/web_static/current',
-  provider => shell,
-}
-exec { 'change_owner':
-  command  => 'chown -R ubuntu:ubuntu /data',
-  provider => shell,
-}
-exec { 'append_location':
-  command  => 'sed -i "/:80 default_server/a location /hbnb_static/ {\n\talias /data/web_static/current/;\n}\n" /etc/nginx/sites-available/default',
-  provider => shell,
-}
-exec { 'sustitute_path':
-  command  => 'sed -i "s:/var/www/html:/data/web_static/current:" /etc/nginx/sites-available/default',
-  provider => shell,
-}
-exec { 'nginx_restart':
-  command  => 'service nginx restart',
+exec { 'prepare_servers':
+  command  => 'apt-get -y update; apt-get -y install nginx; mkdir -p /data/web_static/releases/; mkdir -p /data/web_static/shared/; mkdir -p /data/web_static/releases/test/; echo "<html><head></head><body><h1>Hello Page!</h1></body></html>" > index.html; mv index.html /data/web_static/releases/test/; ln -sf /data/web_static/releases/test /data/web_static/current; chown -R ubuntu:ubuntu /data/; sed -i "/:80 default_server/a location /hbnb_static/ {\n\talias /data/web_static/current/;\n}\n" /etc/nginx/sites-available/default; sed -i "s:/var/www/html:/data/web_static/current:" /etc/nginx/sites-available/default; service nginx restart',
   provider => shell,
 }
